084e82109547091c78114113ddfaa7c198c7220e
rishinaithani <rishinaithani@gmail.com>
2021-05-23 21:53:31 -0400

    Time: 1 ms (100.00%), Space: 47.7 MB (78.73%) - LeetHub

--- bulb-switcher-iii/bulb-switcher-iii.java
+++ bulb-switcher-iii/bulb-switcher-iii.java
@@ -1,6 +1,6 @@
 class Solution {
    
-public int numTimesAllBlue(int[] light) {
+/*public int numTimesAllBlue(int[] light) {
         int max=light[0];
         int ans=0;
         for(int i=0;i<light.length; i++){
@@ -11,5 +11,20 @@ public int numTimesAllBlue(int[] light) {
                 ans++;
         }
         return ans;
-    }
+    } */
+    
+    
+    
+    public int numTimesAllBlue(int[] light) {
+	int switchedOnbulbsSum = 0, count = 0, indexSum = 0;
+
+	for(int i=0; i<light.length; i++) {
+		switchedOnbulbsSum+= light[i];
+		indexSum += i+1;
+
+		if(switchedOnbulbsSum == indexSum) count++;
+	}
+
+	return count;
+}
 }
